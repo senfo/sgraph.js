876f4832d7bc9870efa6643c76b7d639cd012d86
Sean <sean.fao@gmail.com>
2011-12-14 18:44:47 -0500

    Added a license and the initial stub for the graphdb class.

---
+++ code/graphdb.js
@@ -0,0 +1,15 @@
+/*  sgraph.js JavaScript framework, version 0.0.1
+ *  (c) 2011 Sean Fao <sfao@42six.com>
+ *
+ *  sgraph is freely distributable under the terms of the Microsoft Public
+ *  License (Ms-PL). For details, please see LICENSE.TXT at the root of this
+ *  project.
+ *
+/*--------------------------------------------------------------------------*/
+
+var GraphDb = Class.create();
+
+Graph.prototype = {
+	initialize: function() {
+	}
+};
